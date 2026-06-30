f79e6adaae26bc818d0095564c675efa2411cd7b
itsranjithkumar <163500531+itsranjithkumar@users.noreply.github.com>
2026-06-30 23:35:02 +0530

    Complete exercises 4.3-4.4

--- bloglist-backend/tests/list_helper.test.js
+++ bloglist-backend/tests/list_helper.test.js
@@ -1,4 +1,4 @@
-const { test } = require('node:test')
+const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
@@ -8,4 +8,49 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
 
   assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
+  const emptyList = []
+
+  const listWithOneBlog = [
+    {
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      likes: 5
+    }
+  ]
+
+  const listWithManyBlogs = [
+    {
+      title: 'Blog 1',
+      author: 'Author A',
+      likes: 5
+    },
+    {
+      title: 'Blog 2',
+      author: 'Author B',
+      likes: 10
+    },
+    {
+      title: 'Blog 3',
+      author: 'Author C',
+      likes: 15
+    }
+  ]
+
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes(emptyList)
+    assert.strictEqual(result, 0)
+  })
+
+  test('when list has one blog equals its likes', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
+  test('of bigger list equals sum of likes', () => {
+    const result = listHelper.totalLikes(listWithManyBlogs)
+    assert.strictEqual(result, 30)
+  })
 })
